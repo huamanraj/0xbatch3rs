15d5923e8ae0976ffdc337bb4d510f28b6a31689
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2024-12-25 22:15:50 +0530

    feed components added

---
+++ src/components/FeaturedBat/featuredBats.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+function FeaturedBats() {
+  return (
+    <div>featured-bats</div>
+  )
+}
+
+export default FeaturedBats
---
+++ src/components/TopCreatorCard/TopCreatorCard.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+function TopCreatorsCard() {
+  return (
+    <div>TopCreatorCard</div>
+  )
+}
+
+export default TopCreatorsCard
